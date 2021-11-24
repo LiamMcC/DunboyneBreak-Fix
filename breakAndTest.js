3e5069b99f8b37c7fd5281bc4ea36d0f4814babd
LiamMcC <me@liammccabe.ie>
2021-11-24 11:50:00 +0000

    Update breakAndTest.js

--- breakAndTest.js
+++ breakAndTest.js
@@ -1,34 +1,37 @@
-var Person = require("./makeup.js");
-var Vehicle = require("./idea.js");
-// Checks whether minimum of 3 parameters have been entered
-// 
-if(process.argv.length <= 1) {
-  console.log("You must pass the age of the person.");
-  // Exists 
-  process.exit();
+// This class is due to return information to car owners
+// it is based on the fuel they input
+// this should return a message to the user based on the method that is called
+
+class Fuel {
+    constructor(fuelName, price, fuelType) {
+        this.name = fuelFame;
+        this.origin = price;
+        this.furlType = fuelType;
+    }
+
+
+    getfuelPrice() {
+      
+        // here needs to be some code to tell the price of this fuel
+    }
+
+    getFuelDetails {
+
+
+        // here needs to be some code to tell the all the details of this fuel
+
+    }
+    
+   getRecommendation {}
+
+
+   // here needs to be some code that returns a recommendation on the car owner going forward
+   
+
 }
-// Creates the person using the constructor function
-// 
-var person = new Person(2,3, false);
 
-console.log("Person is an adult is: " + person.isAdult());
-console.log("Person can have bank account is: " + person.canHaveBankAccounts());
-// Sets the age of the person
-// 
-person.setAge(process.argv[2]);
+var actor = new Tractor("Liam", Navan, "Action");
+console.log(actor.getGenre()) 
+console.log(actor2.getActorDetails)
 
-console.log("Person is an adult is: " + person.isAdult());
-console.log("Person can have bank account is: " + person.canHaveBankAccounts());
-
-var car1 = new Vehicle("Toyota", "Avensis", 2017);
-const car2 = new Vehicle("Ford", "Focus", 2001);
-
-console.log("Car 2 is a " + car2.getAge() + " year old " + car2.make)
-
-
-function liam (){
-  var car1 = new Vehicle("Toyota", "Avensis", 2017);
-  console.log("Function: Car 1 is a " + car1.getAge() + " year old " + car1.make)
-}
-
-liam()
+// you must comment the code correctly 
